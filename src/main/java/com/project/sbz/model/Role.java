cae907147955b8de49ca1c04551464c626fdb837
Milosh94 <milossm94@hotmail.com>
2017-07-07 19:56:05 +0200

    Initial commit

---
+++ src/main/java/com/project/sbz/model/Role.java
@@ -0,0 +1,66 @@
+package com.project.sbz.model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class Role implements Serializable{
+	
+	private static final long serialVersionUID = 4754741039748241739L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@JsonBackReference
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "role")
+	private Set<User> users;
+	
+	public Role() {
+		
+	}
+
+	public Role(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/project/sbz/model/User.java
@@ -0,0 +1,125 @@
+package com.project.sbz.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.project.sbz.serializer.JsonDateSerializer;
+
+@Entity
+public class User implements Serializable{
+
+	private static final long serialVersionUID = -8248033977651208961L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	
+	private String password;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@JsonManagedReference
+	@JoinColumn(nullable = false, name = "role_id")
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Role role;
+	
+	@JsonSerialize(using = JsonDateSerializer.class)
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "register_date")
+	private Date registerDate;
+	
+	public User() {
+		
+	}
+
+	public User(int id, String username, String password, String firstName, String lastName, Role role,
+			Date registerDate) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.role = role;
+		this.registerDate = registerDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+	
+	
+	
+}
